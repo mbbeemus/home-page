1d4062ebc587a892381fdc107c7fea308024943c
mallory-beemus <162145723+mallory-beemus@users.noreply.github.com>
2025-10-31 14:34:14 -0400

    lab 10

--- catalog/admin.py
+++ catalog/admin.py
@@ -6,4 +6,16 @@ admin.site.register(Genre)
 admin.site.register(Book)
 
 @admin.register(BookInstance)
-clas
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
+
