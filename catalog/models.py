fa632f98ed6c7002f1f51f5944775e9b0de049f0
mallory-beemus <162145723+mallory-beemus@users.noreply.github.com>
2025-11-09 21:01:38 -0500

    lab_9

--- catalog/models.py
+++ catalog/models.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from datetime import date
 
 
+
 class Genre(models.Model):
     """Book genre (e.g. Science Fiction, Poetry)."""
     name = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction)")
@@ -67,5 +68,15 @@ class Author(models.Model):
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField('died', null=True, blank=True)
 
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        """Returns the URL to access a particular author instance."""
+        return reverse('author-detail', args=[str(self.id)])
+
+
     def __str__(self):
         return f'{self.last_name}, {self.first_name}'
+
+
